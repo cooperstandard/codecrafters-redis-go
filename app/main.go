9ed69a47349821c52931b2e2404ebb4491757c5d
Cooper Standard <cooper@cooperstandard.org>
2026-03-14 13:55:11 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -21,9 +21,17 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	conn.Write(byteEncodeString("PONG"))
+
+
+}
+
+func byteEncodeString(input string) []byte {
+	return []byte(fmt.Sprintf("+%s\r\n", input))
 }
