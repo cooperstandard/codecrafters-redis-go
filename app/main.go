e39db8348091239d31ebb0e2ebd9ca41adf161bf
Cooper Standard <cooper@cooperstandard.org>
2026-03-14 14:50:48 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -6,10 +6,6 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -21,26 +17,32 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
 	}
+}
+
+func handleConnection(conn net.Conn) {
+	var err error = nil
 
 	buffer := make([]byte, 1024)
 
 	for {
-		_, err =	conn.Read(buffer)
+		_, err = conn.Read(buffer)
 		if err != nil {
 			break
 		}
 
 		conn.Write(byteEncodeString("PONG"))
 	}
-	
-
-
-
 }
 
 func byteEncodeString(input string) []byte {
