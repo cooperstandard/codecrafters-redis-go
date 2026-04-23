98c91a998ef258e31198960b086893d417fe602f
Cooper Standard <cooper@cooperstandard.org>
2026-04-22 18:40:53 -0700

    add new helper

--- app/main.go
+++ app/main.go
@@ -17,13 +17,13 @@ func main() {
 	port := 6379
 
 	if len(os.Args) > 1 {
-		//TODO: real arg parsing
+		// TODO: real arg parsing
 		port, _ = strconv.Atoi(os.Args[2])
 	}
 
 	// Uncomment the code below to pass the first stage
 	//
-	l, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d",port))
+	l, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", port))
 	if err != nil {
 		fmt.Printf("Failed to bind to port %d\n", port)
 		os.Exit(1)
@@ -58,11 +58,16 @@ func handleConnection(conn net.Conn, config parser.Config) {
 		cmd := buffer[:n]
 		command, args := parser.ParseString(cmd)
 		if _, exists := config.Queues[conn]; exists && !parser.DoesCommandEndTransaction(command) {
-			config.Queues[conn] = append(config.Queues[conn], parser.QueuedCommand{ Args: args, Callback: command.Callback})
+			config.Queues[conn] = append(config.Queues[conn], parser.QueuedCommand{Args: args, Callback: command.Callback})
 			conn.Write(parser.GetSimpleString("QUEUED"))
 		} else {
 			output := command.Callback(args, conn, config)
 			conn.Write(output)
 		}
 	}
+}
+
+func parseFlags(flags string) map[string]string {
+	// val := prior[:2]
+	return make(map[string]string)
 }
